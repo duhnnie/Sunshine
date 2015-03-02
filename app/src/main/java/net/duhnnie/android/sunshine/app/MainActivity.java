b7bd531b06a6e5211d425b78bae482104dbbda51
duhnnie <dnl.canedo@gmail.com>
2015-03-02 00:47:45 -0400

    Create some fake data

--- app/src/main/java/net/duhnnie/android/sunshine/app/MainActivity.java
+++ app/src/main/java/net/duhnnie/android/sunshine/app/MainActivity.java
@@ -11,6 +11,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 
 public class MainActivity extends ActionBarActivity {
 
@@ -60,6 +63,18 @@ public class MainActivity extends ActionBarActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            String[] forecastArray = {
+                "Today - Sunny - 88 /63",
+                "Tomorrow - Foggy - 70 /46",
+                "Weds - Cloudy - 72 / 63",
+                "Thurs - Rainy - 64 / 51",
+                "Fri - Foggy - 70 /46",
+                "Sat - Sunny - 76 / 68"
+            };
+
+            ArrayList<String> weekForecast = new ArrayList<String>(Arrays.asList(forecastArray));
+
             return rootView;
         }
     }

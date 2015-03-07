5858917bdad1d73eced497b89ec77761b5ca8339
duhnnie <dnl.canedo@gmail.com>
2015-03-07 18:19:11 -0400

    ItemClickListenet and Toast

--- app/src/main/java/net/duhnnie/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/net/duhnnie/android/sunshine/app/ForecastFragment.java
@@ -1,5 +1,6 @@
 package net.duhnnie.android.sunshine.app;
 
+import android.content.Context;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -12,8 +13,10 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -85,6 +88,17 @@ public class ForecastFragment extends Fragment {
 
         ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
         listView.setAdapter(mForecastAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Context context = parent.getContext();
+                CharSequence text = (CharSequence) parent.getItemAtPosition(position); //or (CharSequence)((TextView) view).getText()
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
 
         return rootView;
     }

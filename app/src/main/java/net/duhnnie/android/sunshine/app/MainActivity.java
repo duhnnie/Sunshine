fb2bd80c60c700f614b29427d1439a1365116d27
duhnnie <dnl.canedo@gmail.com>
2015-03-19 01:24:06 -0400

    Launch SettingsActivity

--- app/src/main/java/net/duhnnie/android/sunshine/app/MainActivity.java
+++ app/src/main/java/net/duhnnie/android/sunshine/app/MainActivity.java
@@ -1,5 +1,6 @@
 package net.duhnnie.android.sunshine.app;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
@@ -54,6 +55,7 @@ public class MainActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
             return true;
         }
 

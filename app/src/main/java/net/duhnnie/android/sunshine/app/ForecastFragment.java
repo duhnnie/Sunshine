06c31d0b4962d1bb3d358617bde228590658db26
duhnnie <dnl.canedo@gmail.com>
2015-05-25 11:39:35 -0400

    4.c Leveraging Projections

--- app/src/main/java/net/duhnnie/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/net/duhnnie/android/sunshine/app/ForecastFragment.java
@@ -1,7 +1,7 @@
 package net.duhnnie.android.sunshine.app;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
+//import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
@@ -43,6 +43,35 @@ public class ForecastFragment extends Fragment implements LoaderManager.LoaderCa
 
     private ForecastAdapter mForecastAdapter;
     private static int FORECAST_LOADER = 0;
+    private static final String[] FORECAST_COLUMNS = {
+            // In this case the id needs to be fully qualified with a table name, since
+            // the content provider joins the location and weather tables in the background
+            // (both have an _id column)
+            // On the one hand, that's annoying, on the other you can search the Weather table using
+            // the location set by the user, which is only in the Location table.
+            // So the convenience is worth.
+            WeatherContract.WeatherEntry.TABLE_NAME + "."
+                    + WeatherContract.WeatherEntry._ID,
+            WeatherContract.WeatherEntry.COLUMN_DATE,
+            WeatherContract.WeatherEntry.COLUMN_SHORT_DESC,
+            WeatherContract.WeatherEntry.COLUMN_MAX_TEMP,
+            WeatherContract.WeatherEntry.COLUMN_MIN_TEMP,
+            WeatherContract.LocationEntry.COLUMN_LOCATION_SETTING,
+            WeatherContract.WeatherEntry.COLUMN_WEATHER_ID,
+            WeatherContract.LocationEntry.COLUMN_COORD_LAT,
+            WeatherContract.LocationEntry.COLUMN_COORD_LONG
+    };
+
+    //These indices are tied to the FORECAST_COLUMNS. if FORECAST_COLUMNS changes these must change.
+    static final int COL_WEATHER_ID = 0;
+    static final int COL_WEATHER_DATE = 1;
+    static final int COL_WEATHER_DESC = 2;
+    static final int COL_WEATHER_MAX_TEMP = 3;
+    static final int COL_WEATHER_MIN_TEMP = 4;
+    static final int COL_LOCATION_SETTING = 5;
+    static final int COL_WEATHER_CONDITION = 6;
+    static final int COL_COORD_LAT = 7;
+    static final int COL_COORD_LONG = 8;
 
     public ForecastFragment() {
     }
@@ -77,21 +106,6 @@ public class ForecastFragment extends Fragment implements LoaderManager.LoaderCa
         updateWeather();
     }
 
-    public void showLocationOnMap() {
-        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
-        String location = prefs.getString(
-                getString(R.string.pref_location_key),
-                getString(R.string.pref_location_default));
-        Intent newIntent = new Intent(Intent.ACTION_VIEW);
-        Uri geolocation = Uri.parse("geo:0,0?").buildUpon()
-                .appendQueryParameter("q", location)
-                .build();
-        newIntent.setData(geolocation);
-        if (newIntent.resolveActivity(getActivity().getPackageManager()) != null) {
-            startActivity(newIntent);
-        }
-    }
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
@@ -99,12 +113,9 @@ public class ForecastFragment extends Fragment implements LoaderManager.LoaderCa
             case R.id.action_refresh:
                 updateWeather();
                 break;
-            case R.id.action_map:
-                showLocationOnMap();
         }
         return super.onOptionsItemSelected(item);
     }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -131,7 +142,14 @@ public class ForecastFragment extends Fragment implements LoaderManager.LoaderCa
                locationSetting, System.currentTimeMillis()
        );
 
-       return new CursorLoader(getActivity(), weatherForLocationUri, null, null, null, sortOrder);
+       return new CursorLoader(
+               getActivity(),
+               weatherForLocationUri,
+               FORECAST_COLUMNS,
+               null,
+               null,
+               sortOrder
+       );
 
        /*return getActivity().getContentResolver().query(weatherForLocationUri,
                null, null, null, sortOrder);*/

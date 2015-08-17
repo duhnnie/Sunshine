7167377fc87971bf0198ebd3dc359922933d612c
duhnnie <dnl.canedo@gmail.com>
2015-08-17 11:04:11 -0400

    05. Polishing

--- app/src/main/java/net/duhnnie/android/sunshine/app/Utility.java
+++ app/src/main/java/net/duhnnie/android/sunshine/app/Utility.java
@@ -68,12 +68,13 @@ public class Utility {
         // If the date we're building the String for is today's date, the format
         // is "Today, June 24"
         if (julianDay == currentJulianDay) {
-            String today = context.getString(R.string.today);
-            int formatId = R.string.format_full_friendly_date;
-            return String.format(context.getString(
-                    formatId,
-                    today,
-                    getFormattedMonthDay(context, dateInMillis)));
+            //String today = context.getString(R.string.today);
+            //int formatId = R.string.format_full_friendly_date;
+//            return String.format(context.getString(
+//                    formatId,
+//                    today,
+//                    getFormattedMonthDay(context, dateInMillis)));
+            return context.getString(R.string.today);
         } else if ( julianDay < currentJulianDay + 7 ) {
             // If the input date is less than a week in the future, just return the day name.
             return getDayName(context, dateInMillis);
